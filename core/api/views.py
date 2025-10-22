e990670b77f4afed8377181082e3b5dcc27b1710
anmamun0 <anmamun0@gmail.com>
2025-10-22 18:53:19 +0600

    feat: add DRF video 03 practice - nested serializers, SerializerMethodField, and serializer relations

--- core/api/views.py
+++ core/api/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import get_object_or_404
 from api.serializers import ProductSerializer
-from api.models import Product
+from api.models import Product,  Order, OrderItem
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from api.serializers import ProductSerializer, OrderSerializer
 
 @api_view(['GET'])
 def product_list(request):
@@ -15,4 +15,11 @@ def product_list(request):
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
     serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def order_list(request):
+    orders = Order.objects.all()
+    serializer = OrderSerializer(orders, many=True)
     return Response(serializer.data)
