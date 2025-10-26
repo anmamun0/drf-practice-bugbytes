fd242670a8685a7ad025d0d73ed19db41f0f53cd
anmamun0 <anmamun0@gmail.com>
2025-10-26 15:45:27 +0600

    docs: 13 - jwt Refresh Tokens and Setting Management

--- core/core/settings.py
+++ core/core/settings.py
@@ -135,3 +135,14 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
+
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
+    
+    "AUTH_HEADER_TYPES": ("Bearer",), 
+    "USER_ID_FIELD": "id",
+    "USER_ID_CLAIM": "user_id",
+}
